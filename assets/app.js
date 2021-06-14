81ac41f6159d15ce5f313db630b0fe20c0d842ef
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2021-06-14 18:48:55 -0400

    node.js

--- assets/app.js
+++ assets/app.js
@@ -1,2 +1,21 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
+
+// const printProfileData = (profileDataArr) => {
+//   for (let i = 0; i < profileDataArr.length; i++) {
+//     console.log(profileDataArr[i]);
+//   }
+// };
+// // Is the same as this...
+// profileDataArr.forEach((profileItem) => {
+//   console.log(profileItem);
+// });
+
+// printProfileData(profileDataArgs);
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+
+const generatePage = (userName, githubName) =>
+  `Name: ${userName}, Github: ${githubName}`;
+console.log(generatePage('Jane', 'janehub'));
