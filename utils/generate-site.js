6f5137de2c01526247b18461854d478ea5dfa7ac
Farnoush <farnoush.yousefikejani@uwaterloo.ca>
2022-08-07 17:32:27 -0400

    update the JSON

---
+++ utils/generate-site.js
@@ -0,0 +1,39 @@
+//The fs.writeFile() function definition has three arguments. The first argument is the name of the file that's being created. The next argument is the data that will write onto the file, in this case the HTML template literal. The last parameter is a callback function that will be used for error handling.
+
+const fs = require('fs');
+
+// writing files
+const writeFile = fileContent => {
+  return new Promise((resolve, reject) => {
+    fs.writeFile('./dist/index.html', fileContent, err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'File created!'
+      });
+    });
+  });
+};
+
+// copying file
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      resolve({
+        ok: true,
+        message: 'Stylesheet created!'
+      });
+    });
+  });
+};
+
+module.exports = { writeFile, copyFile };
